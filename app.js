9cfbd5d5592c85ef3fbb41c723bc65b7707bfa45
Mohammad Khader <khaderwaseem7186@gmail.com>
2020-12-20 09:08:58 -0500

    Full Pipeline Test

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('AWSCloud!'));
+app.get('/', (req, res) => res.send('AmazonCloud!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
+
+
